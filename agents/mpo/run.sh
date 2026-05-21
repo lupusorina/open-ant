1bcf63a0f8d0c111c17f0267391f436ff8d218d0
Sorina <lupusorina@yahoo.com>
2026-05-21 14:41:10 -0700

    add to run in parallel and the continual learning

--- agents/mpo/run.sh
+++ agents/mpo/run.sh
@@ -1,30 +1,51 @@
 #!/bin/bash
-
 # Learn in simulation.
 if [ "$1" == "sim" ]; then
     if [ "$2" == "multi_seed" ]; then
-       SEEDS=(459 3956 1079 2205 4086 3849 4996 4998 3166 1798)
+        SEEDS=(2 3 4 5 6 7 8 9 10)
     else
-        SEEDS=(1)
+        SEEDS=(0)
     fi
+
     for SEED in "${SEEDS[@]}"; do
         python3 mpo_default.py \
             --render_mode rgb_array \
             --total_timesteps 40000 \
-            --dt 0.15 \
+            --dt 0.12 \
             --env_id SimEmbodiedAnt \
             --runs_directory runs \
             --exp_name retrace \
             --utd_ratio 3 \
             --ensemble 3 \
             --decouple_q_learning \
-            --policy_learning_starts 1000 \
+            --policy_learning_starts 2000 \
             --td_horizon 3 \
             --seed $SEED \
-            # --weights_path /Users/mathieudecker/embodied-mujoco-ant/agents/mpo/runs/seed_459_trial_helios_20260507-180606_seed_459/weights_and_args \
-            # --eval
-        echo "trial ${SEED} done"
+            --cuda &   # <-- background each process
     done
+
+    wait   # <-- block until all seeds finish
+    echo "All seeds done"
+fi
+
+
+if [ "$1" == "sim_continual_learning" ]; then
+    python3 mpo_default.py \
+        --render_mode rgb_array \
+        --env_id SimEmbodiedAnt \
+        --total_timesteps 120_000 \
+        --dt 0.12 \
+        --runs_directory runs \
+        --exp_name retrace_continual_learning \
+        --utd_ratio 3 \
+        --ensemble 3 \
+        --decouple_q_learning \
+        --policy_learning_starts 2000 \
+        --td_horizon 3 \
+        --weights_path /home/sorina/linc/open-ant/agents/mpo/runs/retrace_20260521-121134_seed_0/weights_and_args \
+        --model_path ../../sim/assets/ant_with_camera_after_sys_id_real_less_aggresive.xml \
+        --cuda \
+        --seed 0
 fi
 
 # Learn on hardware.
