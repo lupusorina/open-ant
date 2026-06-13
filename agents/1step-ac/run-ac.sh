293318854e99ee083bd46f0efcc79dbecb6e6190
Sorina Lupu <lupusorina@yahoo.com>
2026-06-13 14:36:01 -0700

    fix run-ac to match the actual onestep_ac_v2 code

--- agents/1step-ac/run-ac.sh
+++ agents/1step-ac/run-ac.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
-SEEDS=(0 1 2 3 4 5)
+SEEDS=(0)
 
 for SEED in "${SEEDS[@]}"; do
 
     echo "Running seed $SEED: one-step actor-critic on CartPole..."
 
-    python3 onestep_ac.py \
+    python3 onestep_ac_v2.py \
         --exp_name one_step_ac \
         --run_dir runs_cartpole/experiment5 \
-        --num_episodes 1000 \
+        --num_episodes 5000 \
         --seed $SEED \
-        --actor_lr 6.8e-05 \
-        --critic_lr 0.0117 \
+        --actor_lr 0.0001 \
+        --critic_lr 0.0001 \
         --gamma 0.99 \
-        --use_layer_norm False 
+        --no-use_layer_norm \
+        --capture_video \
+        --save_video_every_n_episodes 20
 
     echo "Done with seed $SEED"
 done
